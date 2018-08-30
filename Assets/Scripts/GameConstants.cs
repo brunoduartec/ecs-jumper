16e0046d4109fc9c2c5de504b795db93c3226ed5
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2018-08-29 23:03:27 -0300

    update game constants

---
+++ Assets/Scripts/GameConstants.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameConstants : MonoBehaviour
+{
+    public int minX;
+    public int maxX;
+    public int minY;
+    public int maxY;
+    public int blockSize;
+    public int blocksTogether;
+    public int distanceHeightBetweenRows;
+    public int jumpVelocityY;
+    public int jumpVelocityX;
+
+    public int playerInitX;
+    public int playerInitY;
+}
