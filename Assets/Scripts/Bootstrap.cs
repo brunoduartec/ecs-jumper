7138a227f39af4629adb5f11aa3806fe093be797
Bruno Duarte Corrêa <bruno.duartec@gmail.com>
2018-08-30 23:53:17 -0300

    Starting HudSystem

--- Assets/Scripts/Bootstrap.cs
+++ Assets/Scripts/Bootstrap.cs
@@ -23,8 +23,10 @@ public class Bootstrap : MonoBehaviour
 
         var entityManager = World.Active.GetOrCreateManager<EntityManager>();
 
+        World.Active.GetOrCreateManager<UpdateHudSystem>().SetupGameObjects();
+
         Entity player = EntityFactory.Instance.createEntityByName("player");
-        entityManager.SetComponentData(player, new Position { Value = new float3(constants.playerInitX, constants.playerInitY, 0.0f) });
+        entityManager.SetComponentData(player, new Position { Value = new float3(0, constants.minY + constants.blockSize, 0.0f) });
 
         LevelGenerator generator = new LevelGenerator(
             constants.minX,
@@ -35,13 +37,27 @@ public class Bootstrap : MonoBehaviour
             constants.blocksTogether,
             constants.blockSize);
 
-        List<float3> level = generator.buildLevel();
+        List<float3> level = generator.buildBlocks();
 
         foreach (var blockPosition in level)
         {
             Entity block = EntityFactory.Instance.createEntityByName("block");
             entityManager.SetComponentData(block, new Position { Value = blockPosition });
         }
+
+
+        for (int i = constants.minX; i < constants.maxX; i += constants.blockSize)
+        {
+            Entity block = EntityFactory.Instance.createEntityByName("block");
+            float3 blockPosition = new float3(i, constants.minY, 0);
+            entityManager.SetComponentData(block, new Position { Value = blockPosition });
+        }
+
+        Entity score = EntityFactory.Instance.createEntityByName("score");
+        entityManager.SetComponentData(score, new Points { Value = 0 });
+        entityManager.SetComponentData(score, new MaxHeight { Value = 0 });
+
+
     }
 
     // Update is called once per frame
